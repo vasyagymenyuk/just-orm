af7ceb63217e01102bb67eb929b79220b2a3e604
vasyagymenyuk <vasyagymenyuk@yandex.ru>
2022-12-20 01:32:33 +0600

    deleted test file

--- src/modules/run_server.ts
+++
@@ -1,8 +0,0 @@
-import {IServerOptions} from "../interfaces/server.js";
-
-export function runServer(options:IServerOptions){
-    options.port ||= 5000
-    options.host ||= 'localhost'
-
-    console.log(`Server is listening to http://${options.host}:${options.port}`)
-}
